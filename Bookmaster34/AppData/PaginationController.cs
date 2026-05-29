ef004912a25899e8153385144a208ef50f2ad92d
vboxuser <vboxuser@WIN10>
2026-05-29 12:06:45 +0300

    классы

---
+++ Bookmaster34/AppData/PaginationController.cs
@@ -0,0 +1,75 @@
+﻿using Bookmaster34.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookmaster34.AppData
+{
+    /// <summary>
+    /// Обеспечивает навигацию между книгами.
+    /// </summary>
+    internal class PaginationController
+    {
+        /// <summary>
+        /// Список книг.
+        /// </summary>
+        private List<Book> _books = new();
+
+        /// <summary>
+        /// Количество книг на странице.
+        /// </summary>
+        private const int PAGE_SIZE = 50;
+
+        /// <summary>
+        /// Порядковый номер страницы.
+        /// </summary>
+        public int CurrentPage { get; set; }
+
+        /// <summary>
+        /// Общее количество страниц.
+        /// </summary>
+        public int TotalPages { get; set; }
+
+        /// <summary>
+        /// Количество книг.
+        /// </summary>
+        public int BooksCount => _books.Count;
+
+        /// <summary>
+        /// Определяет  возможен ли переход на предыдущую страницу.
+        /// </summary>
+        public bool CanGoPrevious => CurrentPage > 1;
+
+        /// <summary>
+        /// Определяет  возможен ли переход на следующую страницу.
+        /// </summary>
+        public bool CanGoNext => CurrentPage < TotalPages;
+
+        public void Load(List<Book> books)
+        {
+            _books = books ?? new List<Book>();//=> Если коллекция "books" пустая (null), то в коллекции "_books" инициализируем новую коллекцию.
+
+            TotalPages = BooksCount == 0 ? 1 : (int)Math.Ceiling(BooksCount / (double)PAGE_SIZE);
+
+            CurrentPage = 1; // устанавливаем номер страницы по  умолчанию
+        }
+
+        public void GoToPage(int page)
+        {
+            CurrentPage = Math.Clamp(page, 1, TotalPages);
+        }
+
+        public List<Book> GetCurrentPage()
+            {
+            return _books.Skip((CurrentPage - 1) * PAGE_SIZE).Take(PAGE_SIZE).ToList();
+            }
+       
+    }
+}
+           
+          
+             
+     
+
