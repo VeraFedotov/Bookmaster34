4d497890ce9c67957aac77b5156e6ceec0e9835d
vboxuser <vboxuser@WIN10>
2026-05-29 12:11:29 +0300

    прописан функционал для окна клиенты

--- Bookmaster34/View/Pages/ManageCustomersPage.xaml.cs
+++ Bookmaster34/View/Pages/ManageCustomersPage.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Bookmaster34.AppData;
+using Bookmaster34.Models;
+using Bookmaster34.View.Windows;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +24,53 @@ namespace Bookmaster34.View.Pages
     /// </summary>
     public partial class ManageCustomersPage : Page
     {
+        private List<Customer> _customers;
+
+        private Customer _selectedCustomer;
         public ManageCustomersPage()
         {
             InitializeComponent();
+
+            _customers = App.GetContext().Customers.ToList();
+            LoadData();
+        }
+
+        private void EditCustomersBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Customer? selectedCustomer = CustomerLv.SelectedItem as Customer;
+
+            if (selectedCustomer != null)
+            {
+                AddEditCustomerWindow addEditCustomerWindow = new AddEditCustomerWindow();
+                addEditCustomerWindow.ShowDialog();
+            }
+            else
+            {
+                FeedbackService.Error("Невозможно открыть окно для редактирования читателя. Сначала выберите его из списка.");
+            }
+        }
+
+        private void AddCustomersBtn_Click(object sender, RoutedEventArgs e)
+        {
+            AddEditCustomerWindow addEditCustomerWindow = new AddEditCustomerWindow();
+            if (addEditCustomerWindow.ShowDialog() == true)
+            {
+                CustomerLv.ItemsSource = _customers = App.GetContext().Customers.ToList();
+            }
+        }
+
+        private void SearchCustomersBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CustomerLv_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            _selectedCustomer = (Customer)CustomerLv.SelectedItem;
+        }
+        private void LoadData()
+        {
+            CustomerLv.ItemsSource = _customers = App.GetContext().Customers.ToList();
         }
     }
 }
